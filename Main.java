3bd7e41ea83d3149348dc0d1512c9cb45e950ecc
agraire <94527096+agraire@users.noreply.github.com>
2022-11-04 14:21:12 +0000

    Exercice 2

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
+
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Hello world! \nAntoine");
+    System.out.print("Ecrire un nombre: ");
+    int nombre = scanner.nextInt();
+    for(int i=1; i<=10 ; i++)
+      {
+            System.out.println(nombre + " x " + i + " = " +nombre * i);
+      }
   }
 }
